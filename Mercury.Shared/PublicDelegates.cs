5b40280c341f162f20d29bff9f02a124f0e9795b
Tracy Rooks <trooks1017@gmail.com>
2021-06-28 07:32:16 -0400

    Add project files.

---
+++ Mercury.Shared/PublicDelegates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercury.Shared
+{
+    public static class GlobalDelegates
+    {
+        public delegate void SimpleMessageEventHandler(object Sender, Mercury.Shared.SimpleMessageReturnArgs e);
+        public delegate void DataAvailableEventHandler(object Sender, Mercury.Shared.DataReturnArgs e);
+        public delegate void TokenAcquiredEventHandler(object Sender, Mercury.Shared.AuthenticationTokenReturnArgs e);
+
+    }
+}
