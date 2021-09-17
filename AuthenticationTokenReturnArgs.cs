c1257e2c78293891ea6e45e560dc8091e1559d15
11Binary <Tracy@11Binary.us>
2021-09-17 09:14:38 -0400

    Add project files.

---
+++ AuthenticationTokenReturnArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mercury.Shared
+{
+    public class AuthenticationTokenReturnArgs
+    {
+        string Token { get; set; }
+
+        string TokenType { get; set; }
+
+
+    }
+}
